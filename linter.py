bbb9bed6464b94afb444e48fc56b17efd0380a80
Schuyler Jager <schuyler@jewlr.com>
2020-01-06 15:27:02 -0500

    Clean up the rest of deprecations and errors

--- linter.py
+++ linter.py
@@ -17,42 +17,38 @@ class Codeclimate(Linter):
     """Provides an interface to codeclimate."""
 
     defaults = {
+        'chdir': "${project}",
+        'executable': 'codeclimate',
         'selector': (
-            'source.css - '
-            'source.go - '
-            'source.haskall - '
-            'source.java - '
-            'source.javascript - '
-            'source.php - '
-            'source.python - '
-            'source.ruby - '
-            'source.scss - '
-            'source.shell - '
+            'source.css, '
+            'source.go, '
+            'source.haskall, '
+            'source.java, '
+            'source.javascript, '
+            'source.php, '
+            'source.python, '
+            'source.ruby, '
+            'source.scss, '
+            'source.shell, '
             'text.html'
         )
     }
-    executable = "codeclimate"
     regex = r'^(?P<line>\d+)(?:-\d+)?:\s(?P<message>.+)$'
     multiline = False
     line_col_base = (1, 1)
     tempfile_suffix = '-'
     error_stream = util.STREAM_BOTH
-    selectors = {}
     word_re = None
-    defaults = {
-        'chdir': "${project}"
-    }
-    inline_settings = None
-    inline_overrides = None
-    comment_re = None
 
     def cmd(self):
         """Construct a cmd to provide a relative path to 'codeclimate analyze'."""
-        result = [self.executable_path, 'analyze']
+        result = ['codeclimate', 'analyze']
         relative_file_name = None
         for folder in self.view.window().folders():
             if self.filename.find(folder) == 0:
                 relative_file_name = self.filename.replace(folder + '/', '')
+        if relative_file_name == None:
+            return result
         result.append(relative_file_name)
         persist.debug(result)
         return result
